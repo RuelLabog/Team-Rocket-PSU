efdee31e9e8877a7bd62335423096547f4d03861
Jamila Andrada <jamilaandrada17@gmail.com>
2020-02-05 16:01:25 +0800

    dm commit

--- NMS-Inventory System/database/migrations/2020_02_05_071237_create_users_table.php
+++ NMS-Inventory System/database/migrations/2020_02_05_071237_create_users_table.php
@@ -14,12 +14,13 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
+            $table->bigIncrements('userid');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('usertype');
             $table->timestamps();
         });
     }
