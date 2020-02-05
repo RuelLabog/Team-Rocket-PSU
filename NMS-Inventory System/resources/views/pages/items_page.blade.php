50f86f2b60d6b8b7ae8607c6d116a81f85b1e844
RuelLabog <yenglabog010199@gmail.com>
2020-02-05 11:41:29 +0800

    Folder Management

---
+++ NMS-Inventory System/resources/views/pages/items_page.blade.php
@@ -0,0 +1,131 @@
+  @extends('admin_template')
+
+
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+
+            <h1 class="m-0 text-dark"><i class="nav-icon fas fa-box"></i> Items</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+<!-- /.row -->
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">
+                  <i class="fas fa-plus mr-2"></i>Add Item
+                </button>
+
+
+                <div class="card-tools">
+                  <div class="input-group input-group-sm" style="width: 150px;">
+                    <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+                    <div class="input-group-append">
+                      <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+
+                <table class="table table-hover text-nowrap">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Item</th>
+                      <th>Category</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Notebooks</td>
+                      <td>School Supplies</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Notebooks</td>
+                      <td>School Supplies</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>Notebooks</td>
+                      <td>School Supplies</td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>Notebooks</td>
+                      <td>School Supplies</td>
+                    </tr>
+                    <tr>
+                      <td>5</td>
+                      <td>Notebooks</td>
+                      <td>School Supplies</td>
+                    </tr>
+                    <tr>
+                      <td>6</td>
+                      <td>Notebooks</td>
+                      <td>School Supplies</td>
+                    </tr>
+                    
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+        <!-- /.row -->
+
+
+
+
+
+
+<!-- add items modal -->
+      <div class="modal fade" id="modal-default">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Add New Item</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>One fine body&hellip;</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
+      <!-- /.add items modal -->
+
+
+ @endsection
+
+
---
+++ NMS-Inventory System/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages/dashboard');
+});
+
+Route::get('/login', function () {
+    return view('pages/login');
+});
+
+Route::get('/items', function () {
+    return view('pages/items_page');
+});
+
+Route::get('/categories', function () {
+    return view('pages/categories_page');
+});
