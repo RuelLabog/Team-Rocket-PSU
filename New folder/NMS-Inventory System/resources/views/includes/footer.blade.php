45065c13a328643d2b38659271fd0526352dc6c9
Jerry Di V. Eugenio <jerrydieugenio@gmail.com>
2020-02-12 11:59:21 +0800

    Deleted new folder which is accidentally cloned

--- New folder/NMS-Inventory System/resources/views/includes/footer.blade.php
+++
@@ -1,9 +0,0 @@
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      I love you.
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; {{ now()->year }} <a href="https://github.com/RuelLabog/Team-Rocket-PSU">Team Rocket</a>.</strong> All rights reserved.
-  </footer>
